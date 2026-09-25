57309711353e1a10547ff0af61e2ad5358a16d0f
meccies <meccahardy@gmail.com>
2026-09-24 22:08:11 -0700

    Feat: Quit button
    Feature: tweaked transition script between Mecca and Antonio scene, added Quit UI button on win screen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -12,7 +12,7 @@ public class SceneLoader : MonoBehaviour
     }
     
     public void LoadAntonio(){
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("ObstacleCourse");
     }
     
 }
